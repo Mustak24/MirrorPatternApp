6d13734dca3f1598424cfbd65db639fd29a35bcf
Mustak khan <mustak.coc786@gmail.com>
2026-02-13 21:08:02 +0530

    Initial commit

---
+++ src/Shared/Stores/Theme/index.ts
@@ -0,0 +1,18 @@
+import { _colors, _theme } from './constance';
+import { createStore } from '@funtools/store';
+
+const { useStore, useHandlers } = createStore({
+  states: {
+    theme: _theme,
+    colors: _colors[_theme],
+  },
+
+  syncHandlers: {
+    toggleTheme(state) {
+      state.theme = state.theme === 'dark' ? 'light' : 'dark';
+      state.colors = _colors[state.theme];
+    },
+  },
+});
+
+export { useStore as useThemeStore, useHandlers as useThemeHandlers };
